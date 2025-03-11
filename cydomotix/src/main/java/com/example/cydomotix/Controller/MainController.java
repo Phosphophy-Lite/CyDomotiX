8ce899804593a7efdd3441491b1559e768543324
schheeii <schheeii@gmail.com>
2025-03-11 13:25:46 +0100

    Set up project

---
+++ cydomotix/src/main/java/com/example/cydomotix/Controller/MainController.java
@@ -0,0 +1,24 @@
+package com.example.cydomotix.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    /*
+        Controller gérant les requêtes HTTP
+        => Méthodes servent à retourner des vues HTML (nom du fichier HTML)
+     */
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        /* Charge la page home.html quand on accède à la racine "/" du serveur web */
+        return "home";
+    }
+
+}
