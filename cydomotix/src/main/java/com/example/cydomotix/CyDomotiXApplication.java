c90d19a1929a7c98d1fdf354dcddf001df5c97e1
schheeii <schheeii@gmail.com>
2025-03-11 13:09:44 +0100

    Starting project

---
+++ cydomotix/src/main/java/com/example/cydomotix/CyDomotiXApplication.java
@@ -0,0 +1,13 @@
+package com.example.cydomotix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyDomotiXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyDomotiXApplication.class, args);
+	}
+
+}
---
+++ cydomotix/src/test/java/com/example/cydomotix/CyDomotiXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cydomotix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyDomotiXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
