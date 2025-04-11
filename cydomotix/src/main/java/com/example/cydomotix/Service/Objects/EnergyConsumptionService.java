2cfbf5a4d3b29d5d7b2a5df6110d62e1243eaa54
schheeii <schheeii@gmail.com>
2025-04-11 18:55:13 +0200

    * removed useless dependencies and method

--- cydomotix/src/main/java/com/example/cydomotix/Service/Objects/EnergyConsumptionService.java
+++ cydomotix/src/main/java/com/example/cydomotix/Service/Objects/EnergyConsumptionService.java
@@ -151,15 +151,5 @@ public class EnergyConsumptionService {
                 .sum();
     }
 
-
-    public double calculateTotalConsumption(ConnectedObject connectedObject) {
-        List<ConsumptionInterval> intervals = calculateAllConsumptionIntervals(connectedObject);
-
-        return intervals.stream()
-                .filter(interval -> interval.getEnergyWh() > 0) // Ignore les OFF
-                .mapToDouble(ConsumptionInterval::getEnergyWh)
-                .sum();
-    }
-
 }
 
